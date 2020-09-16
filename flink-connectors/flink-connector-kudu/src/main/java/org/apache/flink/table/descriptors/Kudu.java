4f107f8f5329cad00c5e2031583b8f518b784c11
ron.liu <liudalong@bytedance.com>
2020-09-16 22:23:29 +0800

    add kudu connector

---
+++ flink-connectors/flink-connector-kudu/src/main/java/org/apache/flink/table/descriptors/Kudu.java
@@ -0,0 +1,8 @@
+package org.apache.flink.table.descriptors;
+
+/**
+ * @author liudalong
+ * @date 2020/9/15 12:32 上午
+ */
+public class Kudu {
+}
---
+++ flink-connectors/flink-connector-kudu/src/main/java/org/apache/flink/table/descriptors/KuduValidator.java
@@ -0,0 +1,8 @@
+package org.apache.flink.table.descriptors;
+
+/**
+ * @author liudalong
+ * @date 2020/9/15 12:31 上午
+ */
+public class KuduValidator {
+}
